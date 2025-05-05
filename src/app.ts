abd48d83023ca8bbd22a7d78b914d9ec91da7cdb
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-05 22:42:13 +0545

    use express.json to read request

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import config from "config";
 import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
+import categoryRouter from "./category/category-router";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
     res.json({ message: config.get("server.port") });
 });
+app.use(express.json());
+
+app.use("/categories", categoryRouter);
 
 app.use(globalErrorHandler);
 
