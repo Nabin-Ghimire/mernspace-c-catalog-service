64390fd6e6ffba0d9d4fb847ae20aca135e72a6d
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-07 22:07:19 +0545

    add porductRoute

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import categoryRouter from "./category/category-router";
+import producRouter from "./product/product-router";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/categories", categoryRouter);
+app.use("/products", producRouter);
 
 app.use(globalErrorHandler);
 
