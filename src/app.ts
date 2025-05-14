2b6a9b0874b80a2aaf31d8d03b8e9d2c490707bf
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-14 13:33:25 +0545

    express-fileUpload

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import categoryRouter from "./category/category-router";
 import producRouter from "./product/product-router";
 import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
 const app = express();
 
+app.use(fileUpload());
 app.use(express.json());
 app.use(cookieParser());
 
