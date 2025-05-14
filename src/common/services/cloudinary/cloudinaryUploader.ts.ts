359f0c86e30229e17a3c9dd9039f60f1e6b0b13a
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-14 22:16:08 +0545

    cloudinary image delete method

--- src/common/services/cloudinary/cloudinaryUploader.ts.ts
+++ src/common/services/cloudinary/cloudinaryUploader.ts.ts
@@ -4,31 +4,45 @@ import config from "config";
 import path from "path";
 import { unlink } from "fs/promises";
 
-cloudinary.config({
-    cloud_name: config.get("cloudinary.cloudName"),
-    api_key: config.get("cloudinary.apiKey"),
-    api_secret: config.get("cloudinary.apiSecret"),
-});
-
-export async function uploadToCloudinaryAndDeleteLocal(
-    filePath: string,
-): Promise<UploadApiResponse> {
-    const fileName = path.basename(filePath); // use local filename as public_id
-
-    try {
-        const result = await cloudinary.uploader.upload(filePath, {
-            use_filename: true,
-            unique_filename: false,
-            overwrite: true,
-            public_id: fileName,
+export class CloudinaryStorage {
+    constructor() {
+        cloudinary.config({
+            cloud_name: config.get("cloudinary.cloudName"),
+            api_key: config.get("cloudinary.apiKey"),
+            api_secret: config.get("cloudinary.apiSecret"),
         });
+    }
 
-        await unlink(filePath); // Delete local file after successful upload
+    async uploadToCloudinaryAndDeleteLocal(
+        filePath: string,
+    ): Promise<UploadApiResponse> {
+        const fileName = path.basename(filePath); // use local filename as public_id
 
-        return result;
-    } catch (error) {
-        throw new Error(
-            `Cloudinary upload failed: ${(error as Error).message}`,
-        );
+        try {
+            const result = await cloudinary.uploader.upload(filePath, {
+                use_filename: true,
+                unique_filename: false,
+                overwrite: true,
+                public_id: fileName,
+            });
+
+            await unlink(filePath); // Delete local file after successful upload
+
+            return result;
+        } catch (error) {
+            throw new Error(
+                `Cloudinary upload failed: ${(error as Error).message}`,
+            );
+        }
+    }
+
+    async deleteFromCloudinary(publicId: string): Promise<void> {
+        try {
+            await cloudinary.uploader.destroy(publicId);
+        } catch (error) {
+            throw new Error(
+                `Cloudinary delete failed: ${(error as Error).message}`,
+            );
+        }
     }
 }
