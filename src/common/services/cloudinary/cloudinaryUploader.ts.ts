7f2c4e24f4c8170aea90ad994cc39345cd551526
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-14 13:34:18 +0545

    cloudinary setup

---
+++ src/common/services/cloudinary/cloudinaryUploader.ts.ts
@@ -0,0 +1,34 @@
+// utils/cloudinaryUploader.ts
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
+import config from "config";
+import path from "path";
+import { unlink } from "fs/promises";
+
+cloudinary.config({
+    cloud_name: config.get("cloudinary.cloudName"),
+    api_key: config.get("cloudinary.apiKey"),
+    api_secret: config.get("cloudinary.apiSecret"),
+});
+
+export async function uploadToCloudinaryAndDeleteLocal(
+    filePath: string,
+): Promise<UploadApiResponse> {
+    const fileName = path.basename(filePath); // use local filename as public_id
+
+    try {
+        const result = await cloudinary.uploader.upload(filePath, {
+            use_filename: true,
+            unique_filename: false,
+            overwrite: true,
+            public_id: fileName,
+        });
+
+        await unlink(filePath); // Delete local file after successful upload
+
+        return result;
+    } catch (error) {
+        throw new Error(
+            `Cloudinary upload failed: ${(error as Error).message}`,
+        );
+    }
+}
