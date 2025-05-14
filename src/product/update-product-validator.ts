847009c9b1e01d32fe4bb43b766a59945511147c
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-14 22:18:23 +0545

    updateProduct validator

---
+++ src/product/update-product-validator.ts
@@ -0,0 +1,21 @@
+import { body } from "express-validator";
+
+export default [
+    body("name")
+        .exists()
+        .withMessage("Product name is required")
+        .isString()
+        .withMessage("Product name must be a string"),
+
+    body("description").exists().withMessage("Description name is required"),
+
+    body("priceConfiguration")
+        .exists()
+        .withMessage("priceConfiguration name is required"),
+
+    body("attributes").exists().withMessage("Attribute field is required"),
+
+    body("tenantId").exists().withMessage("TenantId field is required"),
+
+    body("categoryId").exists().withMessage("CategoryId field is required"),
+];
