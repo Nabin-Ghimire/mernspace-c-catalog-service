7c20df484eaa0f284e4978c4b5b8b216184eb944
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-17 22:53:13 +0545

    other topping crud operations

--- src/topping/topping-controller.ts
+++ src/topping/topping-controller.ts
@@ -8,6 +8,7 @@ import { Logger } from "winston";
 import { UploadedFile } from "express-fileupload";
 import { saveFileLocally } from "../common/services/multer/localUploader.ts ";
 import { CloudinaryStorage } from "../common/services/cloudinary/cloudinaryUploader.ts";
+import { AuthRequest } from "../common/types";
 
 export class ToppingController {
     constructor(
@@ -70,5 +71,105 @@ export class ToppingController {
             return next(createHttpError(404, "Toppings not found"));
         }
         res.json(toppings);
+    };
+
+    getToppingById = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ) => {
+        const { id } = req.params;
+        const topping = await this.toppingService.getSingleTopping(id);
+        if (!topping) {
+            return next(createHttpError(404, "Topping not found"));
+        }
+        res.json(topping);
+    };
+
+    updateToppingById = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ) => {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return next(createHttpError(400, result.array()[0].msg as string));
+        }
+
+        const { id } = req.params;
+        let cloudinaryResult;
+        let oldImage: string | undefined;
+
+        const topping = await this.toppingService.getSingleTopping(id);
+        if (!topping) {
+            return next(createHttpError(404, "Topping not found"));
+        }
+
+        const tenant = (req as AuthRequest).auth.tenant;
+        if ((req as AuthRequest).auth.role !== "admin") {
+            if (topping.tenantId != String(tenant)) {
+                return next(
+                    createHttpError(
+                        403,
+                        "You are not authorized to update this topping",
+                    ),
+                );
+            }
+        }
+
+        const { name, price, tenantId, isPublish } = req.body as Topping;
+        const file = req.files?.image as UploadedFile;
+        if (file) {
+            oldImage = topping.image;
+            const localPath = await saveFileLocally(file);
+            cloudinaryResult =
+                await this.cloudinaryStorage.uploadToCloudinaryAndDeleteLocal(
+                    localPath,
+                );
+            await this.cloudinaryStorage.deleteFromCloudinary(oldImage);
+        }
+
+        const updateToppingPayload = {
+            name,
+            price,
+            tenantId,
+            isPublish,
+            image: cloudinaryResult?.secure_url
+                ? cloudinaryResult.secure_url
+                : topping.image,
+        };
+
+        await this.toppingService.updateTopping(id, updateToppingPayload);
+        this.logger.info(`Updated topping`, { id });
+        res.json({ id });
+    };
+
+    deleteToppingById = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ) => {
+        const { id } = req.params;
+        const topping = await this.toppingService.getSingleTopping(id);
+        console.log("Image", topping.image);
+        if (!topping) {
+            return next(createHttpError(404, "Topping not found"));
+        }
+
+        const tenant = (req as AuthRequest).auth.tenant;
+        if ((req as AuthRequest).auth.role !== "admin") {
+            if (topping.tenantId != String(tenant)) {
+                return next(
+                    createHttpError(
+                        403,
+                        "You are not authorized to delete this topping",
+                    ),
+                );
+            }
+        }
+        await this.cloudinaryStorage.deleteFromCloudinary(topping.image);
+        await this.toppingService.deleteTopping(id);
+        this.logger.info(`Deleted topping`, { id });
+        res.json({ id });
     };
 }
