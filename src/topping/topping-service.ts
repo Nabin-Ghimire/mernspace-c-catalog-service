8d4bda8eeef8d6c2758f50f1fc22a36b4cb995b2
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-17 22:54:07 +0545

    other topping crud service operations

--- src/topping/topping-service.ts
+++ src/topping/topping-service.ts
@@ -27,4 +27,22 @@ export class ToppingService {
             return toppingModel.aggregatePaginate(aggregate, paginateQuery);
         }
     }
+
+    async getSingleTopping(id: string) {
+        return (await toppingModel.findById(id)) as Topping;
+    }
+
+    async updateTopping(id: string, topping: Topping) {
+        return (await toppingModel.findOneAndUpdate(
+            { _id: id },
+            { $set: topping },
+            { new: true },
+        )) as Topping; //new:true returns new updated document
+
+        // return (await toppingModel.findByIdAndUpdate(id, topping, { new: true })) as Topping;
+    }
+
+    async deleteTopping(id: string) {
+        return (await toppingModel.findByIdAndDelete(id)) as Topping;
+    }
 }
