aa440e3ce9ee91f35c70b53aaa00f22b07bda5f9
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-17 21:47:28 +0545

    getAll topping method

--- src/topping/topping-service.ts
+++ src/topping/topping-service.ts
@@ -1,8 +1,30 @@
 import toppingModel from "./topping-model";
-import { Topping } from "./topping-type";
+import { Filter, PaginateQuery, Topping } from "./topping-type";
 
 export class ToppingService {
     async create(topping: Topping) {
         return (await toppingModel.create(topping)) as Topping;
+    }
+
+    async getAllToppings(
+        q: string,
+        filters: Filter,
+        paginateQuery: PaginateQuery,
+    ) {
+        {
+            const searchQueryRegexp = new RegExp(q, "i"); //q=case insensitive
+            const matchQuery = {
+                ...filters,
+                name: searchQueryRegexp,
+            };
+
+            const aggregate = toppingModel.aggregate([
+                {
+                    $match: matchQuery,
+                },
+            ]);
+
+            return toppingModel.aggregatePaginate(aggregate, paginateQuery);
+        }
     }
 }
