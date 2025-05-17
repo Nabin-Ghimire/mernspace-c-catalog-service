bf4e248a206c8755bd5c06694eb2fb39c5135074
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-17 21:48:36 +0545

    getAll(index) topping method in controller

--- src/topping/topping-controller.ts
+++ src/topping/topping-controller.ts
@@ -2,7 +2,7 @@ import { NextFunction, Response } from "express";
 import { Request } from "express-jwt";
 import { validationResult } from "express-validator";
 import createHttpError from "http-errors";
-import { Topping } from "./topping-type";
+import { Filter, Topping } from "./topping-type";
 import { ToppingService } from "./topping-service";
 import { Logger } from "winston";
 import { UploadedFile } from "express-fileupload";
@@ -22,7 +22,7 @@ export class ToppingController {
             return next(createHttpError(400, result.array()[0].msg as string));
         }
 
-        const { name, price, tenantId } = req.body as Topping;
+        const { name, price, tenantId, isPublish } = req.body as Topping;
         const file = req.files!.image as UploadedFile;
 
         if (!file) return res.status(400).send("No file uploaded");
@@ -38,6 +38,7 @@ export class ToppingController {
             price: Number(price),
             tenantId,
             image: imageUri.secure_url,
+            isPublish,
         };
 
         const topping = await this.toppingService.create(toppingsArgs);
@@ -45,5 +46,29 @@ export class ToppingController {
         this.logger.info(`Created topping`, { id: topping._id });
 
         res.json({ id: topping._id });
+    };
+
+    index = async (req: Request, res: Response, next: NextFunction) => {
+        const { q, tenantId, isPublish } = req.query;
+
+        const filters: Filter = {};
+
+        if (tenantId) filters.tenantId = tenantId as string;
+        if (isPublish) filters.isPublish = true;
+
+        const toppings = await this.toppingService.getAllToppings(
+            q as string,
+            filters,
+            {
+                page: req.query.page ? parseInt(req.query.page as string) : 1,
+                limit: req.query.limit
+                    ? parseInt(req.query.limit as string)
+                    : 10,
+            },
+        );
+        if (!toppings) {
+            return next(createHttpError(404, "Toppings not found"));
+        }
+        res.json(toppings);
     };
 }
